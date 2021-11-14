d8e4112ef2b7ba541351383b36776cd646c5db5d
Tobi B <to-tb@gmx.de>
2021-11-14 18:22:35 +0100

    add basic routing

--- frontend/quickcheck/src/index.js
+++ frontend/quickcheck/src/index.js
@@ -2,13 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from "@chakra-ui/react"
 import Login from './pages/Login'
+import {
+  BrowserRouter as Router,
+  Route, Routes,
+  Link
+ } from 'react-router-dom'
 
 
-
+ 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <Login/>
+
+      <Router>
+        <Link to='/'>Home </Link>
+
+        <Link to='/login'>Login </Link>
+
+        <Routes>
+          <Route path='/login' element={<Login/>}>
+
+          </Route>
+        </Routes>
+
+      </Router>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
