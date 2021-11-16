981b5c77eedba1779155cf193b89cad9e5d16c94
Ines <ine.vogel@googlemail.com>
2021-11-16 15:48:45 +0100

    adds additional testcases to ProjectControllerTest (focus on HTTPMethod.PUT)

--- service/src/test/java/com/tu/FinancialQuickCheck/ProjectControllerTest.java
+++ service/src/test/java/com/tu/FinancialQuickCheck/ProjectControllerTest.java
@@ -1,6 +1,5 @@
 package com.tu.FinancialQuickCheck;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,9 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,25 +27,79 @@ public class ProjectControllerTest {
     private String host = "http://localhost:";
     private String projects = "/projects";
     private String projectIDExistsNot = "/0";
-    private String projectIDExists = "/1";
+    private String projectID1 = "/1";
 
-    @Before
+
+
+    @BeforeEach
     public void initEach(){
         // creates an entry with projectID 1 in test db
-        restTemplate.exchange(
+        String tmp = restTemplate.exchange(
+                host + port + projects,
+                HttpMethod.POST,
+                null,
+                String.class).getBody();
+
+        System.out.println("Before Result: " + tmp);
+
+    }
+
+    @AfterEach
+    public void afterwards(){
+        // should delete all data entries in test db
+        restTemplate.delete(host + port + projects);
+    }
+
+    @Test
+    public void putProjectIdExists() throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(
+                "{\"id\":1,\"name\": \"neuer Bankname\",\"creatorID\":1,\"members\":[1,2,3],\"productAreas\":[]}",
+                headers
+        );
+
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                host + port + projects + projectID1,
+                HttpMethod.PUT,
+                request,
+                String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        //        System.out.println("Response Status: " + response.getStatusCode());
+
+    }
+
+    @Test
+    public void getProjects() throws Exception {
+        ResponseEntity<String> response = restTemplate.exchange(
+                host + port + projects,
+                HttpMethod.GET,
+                null,
+                String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        System.out.println("Response Status: " + response.getStatusCode());
+    }
+
+    @Test
+    public void postProjects() throws Exception {
+        ResponseEntity<String> response = restTemplate.exchange(
                 host + port + projects,
                 HttpMethod.POST,
                 null,
                 String.class);
 
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+//        System.out.println("Response Status: " + response.getStatusCode());
     }
 
-//    @After --> should delete all entries in test db
-
-
-
     @Test
-    public void notAllowedHTTPMethodsProjects() throws Exception {
+    public void notAllowedMethodsProjects() throws Exception {
 
         HttpMethod[] notAllowed = new HttpMethod[]{HttpMethod.PUT, HttpMethod.DELETE};
         for (HttpMethod httpMethod : notAllowed) {
@@ -63,52 +114,8 @@ public class ProjectControllerTest {
         }
     }
 
-
     @Test
-    public void listenHTTPMethodGETProjects() throws Exception {
-        ResponseEntity<String> response = restTemplate.exchange(
-                host + port + projects,
-                HttpMethod.GET,
-                null,
-                String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        System.out.println("Response Status: " + response.getStatusCode());
-    }
-
-
-    @Test
-    public void listenHTTPMethodPOSTProjects() throws Exception {
-        ResponseEntity<String> response = restTemplate.exchange(
-                host + port + projects,
-                HttpMethod.POST,
-                null,
-                String.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-//        System.out.println("Response Status: " + response.getStatusCode());
-    }
-
-
-    @Test
-    public void notAllowedHTTPMethodsProjectId() throws Exception {
-
-        HttpMethod[] notAllowed = new HttpMethod[]{HttpMethod.POST};
-        for (HttpMethod httpMethod : notAllowed) {
-            ResponseEntity<String> response = restTemplate.exchange(
-                    host + port + projects + projectIDExists,
-                    httpMethod,
-                    null,
-                    String.class);
-
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.METHOD_NOT_ALLOWED);
-//            System.out.println("Response Status: " + response.getStatusCode());
-        }
-    }
-
-
-    @Test
-    public void listenHTTPMethodProjectIdDoesNotExist() throws Exception {
+    public void allowedMethodsProjectIdDoesNotExist() throws Exception {
         HttpMethod[] allowed = new HttpMethod[]{HttpMethod.GET, HttpMethod.DELETE};
         for (HttpMethod httpMethod : allowed) {
             ResponseEntity<String> response = restTemplate.exchange(
@@ -122,48 +129,56 @@ public class ProjectControllerTest {
         }
     }
 
+    @Test
+    public void notAllowedMethodsProjectId() throws Exception {
+
+        HttpMethod[] notAllowed = new HttpMethod[]{HttpMethod.POST};
+        for (HttpMethod httpMethod : notAllowed) {
+            ResponseEntity<String> response = restTemplate.exchange(
+                    host + port + projects + projectID1,
+                    httpMethod,
+                    null,
+                    String.class);
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.METHOD_NOT_ALLOWED);
+//            System.out.println("Response Status: " + response.getStatusCode());
+        }
+    }
 
     @Test
-    public void listenHTTPMethodProjectIdExists() throws Exception {
-        HttpMethod[] allowed = new HttpMethod[]{HttpMethod.GET};
+    public void allowedMethodsProjectIdExists() throws Exception {
+        HttpMethod[] allowed = new HttpMethod[]{HttpMethod.GET, HttpMethod.DELETE};
         for (HttpMethod httpMethod : allowed) {
             ResponseEntity<String> response = restTemplate.exchange(
-                    host + port + projects + projectIDExists,
+                    host + port + projects + projectID1,
                     httpMethod,
                     null,
                     String.class);
 
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-                    System.out.println("Response Status: " + response.getStatusCode());
+            System.out.println("Response Status: " + response.getStatusCode());
         }
     }
 
-//    @Test
-//    public void listenHttpPUTProjectIdDoesNotExist() throws Exception {
-//        // TODO: change requestEntity to expected JSON Input
-//        ResponseEntity<String> response = restTemplate.exchange(
-//                host + port + projects + projectIDExistsNot,
-//                HttpMethod.PUT,
-//                null,
-//                String.class);
-//
-//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-//        //        System.out.println("Response Status: " + response.getStatusCode());
-//    }
-//
-//
-//    @Test
-//    public void listenHTTPMethodProjectIdExists() throws Exception {
-//
-//        // TODO: change requestEntity to expected JSON input
-//        ResponseEntity<String> response = restTemplate.exchange(
-//                host + port + projects + projectIDExists,
-//                HttpMethod.PUT,
-//                null,
-//                String.class);
-//
-//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        //        System.out.println("Response Status: " + response.getStatusCode());
-//    }
+    @Test
+    public void putProjectIdDoesNotExist() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(
+                "{ }",
+                headers
+        );
+
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                host + port + projects + projectIDExistsNot,
+                HttpMethod.PUT,
+                request,
+                String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        //        System.out.println("Response Status: " + response.getStatusCode());
+    }
 
 }
