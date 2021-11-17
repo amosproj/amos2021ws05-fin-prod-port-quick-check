5c0bd62d6cdf95ac61fdb9764d5a53bbb9396935
Tobi B <to-tb@gmx.de>
2021-11-17 13:37:29 +0100

    fix spacing

--- frontend/quickcheck/src/pages/ProjectOverview.jsx
+++ frontend/quickcheck/src/pages/ProjectOverview.jsx
@@ -2,26 +2,30 @@ import React from "react";
 
 import Menubar from "../components/Menubar";
 import Card from "../components/card";
-import { Flex } from "@chakra-ui/react";
+import { Flex, VStack } from "@chakra-ui/react";
 // import {Flex, VStack, Heading, Input, Button, FormControl, FormLabel} from "@chakra-ui/react"
 
+const mockProjects = {
+    "hello": "foo" 
+}
 
-function ProjectOverview() {
+
+function ProjectOverview(props) {
 
 
     return (
-    <div>
+        <VStack justifyContent='center' base={1000}>
+
         <Menubar title="Project Overview"></Menubar>
-        <Flex justifyContent='center' base={1000}>
-            <Card title="Project 1" type="Open" link="./Login" productowner="amos" lastopened="November 12"
-></Card>
-            <Card title="Project 2" type="Open"></Card>
+
+            <Card title={mockProjects.hello} type="Open"></Card>
             <Card title="Project 3" type="Open"></Card>
             <Card title="Project 4" type="Open"></Card>
+            <Card title="Project 1" type="Open"></Card>
 
-        </Flex>
 
-        </div>
+        </VStack>
+
     )
 }
 
