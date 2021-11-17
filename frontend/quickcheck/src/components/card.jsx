10eaafcb9346746633313db3268f818323b873aa
Lily <lilyhuegerich@Lill.fritz.box>
2021-11-17 13:05:56 +0100

    card

--- frontend/quickcheck/src/components/card.jsx
+++ frontend/quickcheck/src/components/card.jsx
@@ -1,25 +1,30 @@
 import { Flex, Spacer } from '@chakra-ui/layout'
 import React from 'react'
+import { ChakraComponent, Box, BoxProps } from "@chakra-ui/react"
+import { HStack, Heading, Input, Button, FormControl, FormLabel} from "@chakra-ui/react"
+
 
 function Card(props) {
 
     const greeting = 'Project';
 
     return(
-    <div>
+    <Flex bg='black' w='half' align='center' p={3} justifyContent={'space-between'}>
+
     <header className="Card">
+    <HStack>
         <p style={{"margin": "1px", "padding": "5px"}}>
-          <h1>{props.title}</h1>
+          <heading>{props.title}</heading>
         </p>
-        <a style={{"color": "Blue", "backgroundColor": "white"}}
+        <a style={{"color": "white", "backgroundColor": "blue"}}
           className="App-link" href={props.link} target="_blank">
           Open Project
         </a>
+        </HStack>
       </header>
-</div>
-     );
 
-
+</Flex>
+);
 }
 
 export default Card
