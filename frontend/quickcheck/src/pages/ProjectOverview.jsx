24e222a2a5f67115fa08036622f92b08caa589cd
Tobi B <to-tb@gmx.de>
2021-11-17 16:24:31 +0100

    project page update

--- frontend/quickcheck/src/pages/ProjectOverview.jsx
+++ frontend/quickcheck/src/pages/ProjectOverview.jsx
@@ -2,34 +2,35 @@ import React from "react";
 
 import Menubar from "../components/Menubar";
 import Card from "../components/card";
-import { VStack, Flex, List } from "@chakra-ui/react";
-
-// import {Flex, VStack, Heading, Input, Button, FormControl, FormLabel} from "@chakra-ui/react"
+import { VStack, List } from "@chakra-ui/react";
 
 const mock = {
-  title: "Mocki",
-  lastopened: "10.10.2020",
+  title: "Volksbank berlin brandenburg",
+  type: "Open",
+  link: "./Login",
+  lastEdit: "November 12",
+  created: "July 20",
+  role: 'Consultant'
+};
+
+const mock2 = {
+  title: "ING",
+  type: "Open",
+  link: "./Login",
+  lastEdit: "November 12",
+  created: "July 20",
+  role: 'Consultant'
 };
 
 function ProjectOverview(props) {
   return (
     <VStack justifyContent="center" spacing={10}>
       <Menubar title="Project Overview"></Menubar>
-      <Flex base={1000} flexDir="column">
-        <List spacing={3}>
-          <Card title="Project 4" type="Open"></Card>
-          <Card title="Project 1" type="Open"></Card>
-
-          <Card
-            title="Project 1"
-            type="Open"
-            link="./Login"
-            productowner="Amos"
-            lastopened="November 12"
-            created="July 20"
-          ></Card>
+        <List spacing={3} maxW={800}>
+          <Card project={mock}></Card>
+          <Card project={mock2}></Card>
+          <Card project={mock2}></Card>
         </List>
-      </Flex>
     </VStack>
   );
 }
