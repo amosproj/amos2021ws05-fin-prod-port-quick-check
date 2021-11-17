773aedb64c0afeece56d04145d95fb98fc3308ce
Lily <lilyhuegerich@Lill.fritz.box>
2021-11-17 13:10:28 +0100

    card

--- frontend/quickcheck/src/components/card.jsx
+++ frontend/quickcheck/src/components/card.jsx
@@ -9,18 +9,25 @@ function Card(props) {
     const greeting = 'Project';
 
     return(
-    <Flex bg='black' w='half' align='center' p={3} justifyContent={'space-between'}>
+    <Flex bg='black' w='half' rounded="lg" m="5" align='center' p={3} justifyContent={'space-between'}>
 
     <header className="Card">
     <HStack>
         <p style={{"margin": "1px", "padding": "5px"}}>
-          <heading>{props.title}</heading>
+          <Heading size="md"> {props.title}</Heading>
         </p>
         <a style={{"color": "white", "backgroundColor": "blue"}}
           className="App-link" href={props.link} target="_blank">
           Open Project
         </a>
         </HStack>
+        <p style={{"margin": "1px", "padding": "5px"}}>
+          <Heading size="sm">Product Owner: {props.productowner}</Heading>
+        </p>
+        <p style={{"margin": "1px", "padding": "5px"}}>
+          <Heading size="sm">Last opened: {props.lastopened}</Heading>
+        </p>
+
       </header>
 
 </Flex>
