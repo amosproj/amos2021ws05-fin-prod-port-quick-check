711a2fb6852c2ade1fc0116e18ec74c73efe9d81
Tobi B <to-tb@gmx.de>
2021-11-17 13:08:45 +0100

    menubar: remove some attributes

--- frontend/quickcheck/src/components/Menubar.jsx
+++ frontend/quickcheck/src/components/Menubar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 function Menubar(props) {
   return (
-    <Flex bg='gray.700' w='full' h={20} justifyContent='center' as='nav'>
+    <Flex bg='gray.700' w='full' h={20} justifyContent='center'>
 
-      <Flex align='center' basis={1000} px={5} justifyContent='space-between' alignItems='center'>
+      <Flex align='center' basis={1000} px={5}>
         <Heading size="xl">{props.title}</Heading>
         <Spacer />
         <Avatar ></Avatar>
