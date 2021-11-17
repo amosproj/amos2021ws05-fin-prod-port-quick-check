94d6936c68b5a03e87ea14cc32ba87d31ee6a805
Lily <lilyhuegerich@Lill.fritz.box>
2021-11-17 13:36:19 +0100

    button

--- frontend/quickcheck/src/components/card.jsx
+++ frontend/quickcheck/src/components/card.jsx
@@ -1,33 +1,35 @@
 import { Flex, Spacer, VStack } from '@chakra-ui/layout'
 import React from 'react'
 import { ChakraComponent, Box, BoxProps } from "@chakra-ui/react"
-import { HStack, Heading, Input, Button, FormControl, FormLabel} from "@chakra-ui/react"
+import { HStack, Heading, Text, Input, Button, FormControl, FormLabel} from "@chakra-ui/react"
 
 
 function Card(props) {
-    
+
     const greeting = 'Project';
-    
+
     return(
         <Flex bg='gray.700' w={800} rounded="lg" m="5" align='center' p={3} justifyContent='space-between'>
         <VStack>
         <HStack>
-            <Heading size="md"> {props.title}</Heading>
+            <Heading size="md" color="teal"> {props.title}</Heading>
             <Spacer/>
-            <Button>Edit</Button>
+            <Button
+                type='submit' bg='teal.400' hover={{bg: 'teal.300'}} w='20'
+            >Open</Button>
+
         </HStack>
-        
-        <HStack>
-            <Heading size="sm">Product Owner: {props.productowner}</Heading>
-            <Heading size="sm">Last opened: {props.lastopened}</Heading>
-        </HStack>
+
+        <VStack>
+            <Text size="sm" style={{alignSelf: 'flex-start'}}>Product Owner: {props.productowner}</Text>
+            <Text size="sm" style={{alignSelf: 'flex-start'}}>Last opened: {props.lastopened}</Text>
         </VStack>
-        
-        
-        
+        </VStack>
+
+
+
         </Flex>
         );
     }
-    
+
     export default Card
-    
