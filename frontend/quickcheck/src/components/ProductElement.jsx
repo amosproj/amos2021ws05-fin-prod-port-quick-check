c0c29c2f5ebfacedb8b0baebb40668e5c6509c43
Tobi B <to-tb@gmx.de>
2021-11-19 21:35:17 +0100

    add page skeleton for product area page

---
+++ frontend/quickcheck/src/components/ProductElement.jsx
@@ -0,0 +1,19 @@
+import { HStack } from '@chakra-ui/layout'
+import React from 'react'
+
+function ProductElement(props) {
+    return (
+        <HStack bg='gray.600'>
+            {/* das hier ist eine zeile in der produkt liste, jede zeile hat folgende felder: 
+                - label mit produktname (props.product.name)
+                - button für wirtschaftliche bewertung. 
+                    als button text vielleicht den bewertungsfortschritt (zb. props.product.rating.economic)
+                - button für komplexitätsbewertung
+                - textfeld für anmerkungen
+                - button für nachweise mit text "upload" (vielleicht iconbutton mit schönem icon :) 
+            */}
+        </HStack>
+    )
+}
+
+export default ProductElement
---
+++ frontend/quickcheck/src/pages/ProductArea.jsx
@@ -0,0 +1,34 @@
+import { List, VStack } from '@chakra-ui/layout'
+import React from 'react'
+import ProductElement from '../components/ProductElement'
+
+// beispiel mock für eine produkt, das wird dann als prop an eine produktzeile übergeben
+const mockProduct = {
+    name: "Dispokredit", 
+    rating: {
+        economic: 0.3,
+        complexity: 0.8,  // erstmal nur als progress anzeige 0.8 = 80% fortschritt
+    },
+    remark: "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla"
+}
+ 
+function ProductArea(props) {
+    return (
+        <div>
+            {/* Menubar hier mit namen von produktarea (wird in den props )*/}
+            <VStack justifyContent="center" spacing={10} mt={5}>
+                {/* vielleicht ein header mit dem namen "Products", 
+                daneben ein button "add product" oder "edit products" */}
+                
+                <List>
+                {/* liste von Produktelementen (siehe components) */}
+                <ProductElement product={mockProduct} />
+                </List>
+            </VStack>
+            
+
+        </div>
+    )
+}
+
+export default ProductArea
