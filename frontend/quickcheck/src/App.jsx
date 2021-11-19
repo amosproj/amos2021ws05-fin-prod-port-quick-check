58af60255d2b4c632c0d6976efe7727aa9a3e1f2
Lily <lilyhuegerich@Lill.fritz.box>
2021-11-19 18:29:42 +0100

    tmp

--- frontend/quickcheck/src/App.jsx
+++ frontend/quickcheck/src/App.jsx
@@ -3,20 +3,22 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 import Login from './pages/Login';
 import ProjectOverview from './pages/ProjectOverview';
-
+import ProjectManage from './pages/ProjectManage';
 function App() {
   return (
     <div>
-      <Router>
-        <Link to="/">Home </Link>
-        <Link to="/projects">Projects </Link>
-        <Link to="/login">Login </Link>
+    <Router>
+      <Link to="/">Home </Link>
+      <Link to="/projects">Projects </Link>
+      <Link to="/login">Login </Link>
+      <Link to="/project_manage">Project Manage </Link>
 
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/projects" element={<ProjectOverview />}></Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/projects" element={<ProjectOverview />}></Route>
+        <Route path="/project_manage" element={<ProjectManage />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ frontend/quickcheck/src/components/card_member.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  Box,
+  Center,
+  Heading,
+  Text,
+  Stack,
+  Avatar,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+export default function MemberCard(prop) {
+  return (
+    <Center py={6}>
+      <Box
+        maxW={'945px'}
+        w={'full'}
+
+        boxShadow={'2xl'}
+        rounded={'md'}
+        p={6}
+        overflow={'hidden'}>
+        <Box
+          h={'20px'}
+          bg={'red'}
+          mt={-6}
+          mx={-6}
+          mb={6}
+          pos={'relative'}>
+          Members
+        </Box>
+        <Stack>
+          <Text
+            color={'green.500'}
+            textTransform={'uppercase'}
+            fontWeight={800}
+            fontSize={'sm'}
+            letterSpacing={1.1}>
+            ype: "Project",
+              title: 'Volksbank berlin brandenburg',
+              role: 'Consultant',
+              description: "Project with Volksbank.  Project with Volksbank. Project with Volk
+          </Text>
+
+        </Stack>
+
+      </Box>
+    </Center>
+  );
+}
