6c596e900486c2ac2342f89687c701a71c1c4801
Lily <lilyhuegerich@Lill.fritz.box>
2021-11-19 18:41:11 +0100

    table

--- frontend/quickcheck/src/components/card_member.jsx
+++ frontend/quickcheck/src/components/card_member.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Box, Center, Heading, Text, Stack, Avatar, useColorModeValue } from '@chakra-ui/react';
+import { Table, Thead, Th,  TableCaption, Tr, Td, Tfoot, Tbody, Box, Center, Heading, Text, Stack, Avatar, useColorModeValue } from '@chakra-ui/react';
 
 export default function MemberCard(prop) {
   return (
     <Center py={6}>
       <Box maxW={'945px'} w={'full'} boxShadow={'2xl'} rounded={'md'} p={6} overflow={'hidden'}>
-        <Box h={'20px'} bg={'red'} mt={-6} mx={-6} mb={6} pos={'relative'}>
-          Members
+        <Box h={'20px'} bg={'blue.600'} mt={-6} mx={-6} mb={6} pos={'relative'}>
+
         </Box>
         <Stack>
           <Text
@@ -16,9 +16,38 @@ export default function MemberCard(prop) {
             fontSize={'sm'}
             letterSpacing={1.1}
           >
-            ype: "Project", title: 'Volksbank berlin brandenburg', role: 'Consultant', description:
-            "Project with Volksbank. Project with Volksbank. Project with Volk
+          Members
+
           </Text>
+          <Text color={'gray.500'}><Table variant="simple">
+  <Thead>
+    <Tr>
+      <Th>Name</Th>
+      <Th>Role</Th>
+
+    </Tr>
+  </Thead>
+  <Tbody>
+    <Tr>
+      <Td>inches</Td>
+      <Td>millimetres (mm)</Td>
+
+    </Tr>
+    <Tr>
+      <Td>feet</Td>
+      <Td>centimetres (cm)</Td>
+
+    </Tr>
+    <Tr>
+      <Td>yards</Td>
+      <Td>metres (m)</Td>
+
+    </Tr>
+  </Tbody>
+  <Tfoot>
+
+  </Tfoot>
+</Table></Text>
         </Stack>
       </Box>
     </Center>
