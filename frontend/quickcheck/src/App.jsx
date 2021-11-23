e4e352aaf284ca83a91f894bdce32e1a4cc7f11b
Tobi B <to-tb@gmx.de>
2021-11-23 13:15:31 +0100

    Delete all changes related to the manage project page from this branch.

--- frontend/quickcheck/src/App.jsx
+++ frontend/quickcheck/src/App.jsx
@@ -3,21 +3,18 @@ import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-ro
 
 import Login from './pages/Login';
 import ProjectOverview from './pages/ProjectOverview';
-import ManageProject from './pages/ProjectManage';
 function App() {
   return (
     <div>
       <Router>
         <Link to="/login">| Login </Link>
         <Link to="/projects">| Projects </Link>
-        <Link to="/ManageProject">| Manage Project </Link>
 
         <Routes>
           {/* TODO: use proper redirect (see https://gist.github.com/mjackson/b5748add2795ce7448a366ae8f8ae3bb) */}
           <Route path="/" element={<Navigate replace to="/login" />} />
           <Route path="/login" element={<Login />}></Route>
           <Route path="/projects" element={<ProjectOverview />}></Route>
-          <Route path="/ManageProject" element={<ManageProject />}></Route>
         </Routes>
       </Router>
     </div>
