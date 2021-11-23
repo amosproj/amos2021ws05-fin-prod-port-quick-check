ffb67b88c07dcae37150ecc9a7861806c579f175
Tobi B <to-tb@gmx.de>
2021-11-23 14:38:16 +0100

    fix table

--- frontend/quickcheck/src/components/MemberCard.jsx
+++ frontend/quickcheck/src/components/MemberCard.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {
   Button,
   Link,
-  Flex,
-  List,
   Table,
   Thead,
   Th,
@@ -13,7 +11,6 @@ import {
   Tbody,
   Box,
   Center,
-  Heading,
   Text,
   Stack,
 } from '@chakra-ui/react';
@@ -21,9 +18,7 @@ import { IconButton } from '@chakra-ui/react';
 
 function MemberRow(prop) {
   return (
-    <React.Fragment>
       <Tr>
-        <Center>
           <Td>
             <Box color="white" boxShadow={'2xl'} rounded={'md'} w="200px" bg="blue.500" p={3}>
               <Text color={'gray.100'} fontWeight={800} fontSize={'sm'} letterSpacing={1.1}>
@@ -32,7 +27,7 @@ function MemberRow(prop) {
             </Box>
           </Td>
 
-          <Td isNumeric>
+          <Td>
             <Box color="white" boxShadow={'2xl'} rounded={'md'} w="200px" bg="green.500" p={3}>
               <Text
                 color={'gray.100'}
@@ -45,7 +40,7 @@ function MemberRow(prop) {
               </Text>
             </Box>
           </Td>
-          <Td isNumeric>
+          <Td>
             <Link to="../projects">
               <Button
                 size="lg"
@@ -60,15 +55,12 @@ function MemberRow(prop) {
               </Button>
             </Link>
           </Td>
-        </Center>
       </Tr>
-    </React.Fragment>
   );
 }
 
 export default function MemberCard(props) {
   return (
-    <Center py={6}>
       <Box maxW={'945px'} w={'full'} boxShadow={'2xl'} rounded={'md'} p={6} overflow={'hidden'}>
         <Box h={'20px'} bg={'blue.600'} mt={-6} mx={-6} mb={6} pos={'relative'}></Box>
         <Stack>
@@ -81,27 +73,22 @@ export default function MemberCard(props) {
           >
             Members
           </Text>
-          <Text color={'gray.100'}>
             <Table variant="simple" size="sm">
               <Thead>
                 <Tr>
                   <Th>Name</Th>
-                  <Th isNumeric>Role</Th>
+                  <Th>Role</Th>
                   <Th>Remove</Th>
                 </Tr>
               </Thead>
               <Tbody>
-                <List spacing={3} maxW={800} mx={2}>
                   {props.Members.map((member) => (
                     <MemberRow name={member.Name} role={member.role}></MemberRow>
                   ))}
-                </List>
               </Tbody>
               <Tfoot></Tfoot>
             </Table>
-          </Text>
         </Stack>
       </Box>
-    </Center>
   );
 }
