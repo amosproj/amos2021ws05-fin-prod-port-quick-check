57fcd8aaaed5344e2a63d12385e4d477ac3f6e95
Ines <ine.vogel@googlemail.com>
2021-11-23 09:22:33 +0100

    removes unnecessary imports

--- service/src/test/java/com/tu/FinancialQuickCheck/Controller/ProductAreaControllerTest.java
+++ service/src/test/java/com/tu/FinancialQuickCheck/Controller/ProductAreaControllerTest.java
@@ -1,7 +1,5 @@
 package com.tu.FinancialQuickCheck.Controller;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
