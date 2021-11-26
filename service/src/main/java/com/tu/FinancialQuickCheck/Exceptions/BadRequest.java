3d3c67cea93521bb4fd5bbeee87a5c2c2db2aa91
Ines <ine.vogel@googlemail.com>
2021-11-26 14:09:28 +0100

    adds BadRequest Class

---
+++ service/src/main/java/com/tu/FinancialQuickCheck/Exceptions/BadRequest.java
@@ -0,0 +1,14 @@
+package com.tu.FinancialQuickCheck.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequest extends RuntimeException {
+
+    public BadRequest(String exception){
+            super(exception);
+        }
+
+}
+
