d82b9da746cfb444e8102e52d75fbbd58b28a38f
Andreas Ronellenfitsch <bn0122@partner.kit.edu>
2021-11-28 23:10:32 +0100

    Created first draft of product overview

--- frontend/quickcheck/src/pages/ProductOverview.jsx
+++ frontend/quickcheck/src/pages/ProductOverview.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
-import { SimpleGrid, Box, CircularProgress, CircularProgressLabel, IconButton  } from "@chakra-ui/react"
+import React, { useState, useEffect } from 'react'
 import Page from '../components/Page'
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Wrap, WrapItem, Center } from "@chakra-ui/react"
-import { Textarea, Text } from "@chakra-ui/react"
-import { Button } from '@chakra-ui/button'
+import {
+  Text,
+  Box,
+  Button,
+  List,
+  Heading,
+  HStack,
+  Spacer
+} from "@chakra-ui/react"
+import Card from '../components/Card';
+// import { useToast } from '@chakra-ui/react';
+
+// import { api } from '../utils/apiClient';
+import { Link } from 'react-router-dom';
+
+const prod1mock = {
+  newProject: {
+    creatorID: 0,
+    projectName: 'Karl-Heinz Günther',
+    members: [1, 2],
+    productAreas: [1, 2, 3],
+  },
+  area: 'Loan',
+};
+
+const project1 = {
+  project: {
+    projectName: "TestFName1",
+    projectID: 0,
+  }
+}
+
+const project2 = {
+  project: {
+    projectName: "TestName2",
+    projectID: 1,
+  }
+}
+const projectsData = {}
+/* const projectsData = {
+  project1: {
+    projectName: "Name1",
+    projectID: 0
+  },
+  project2:{
+    projectName: "Name2",
+    projectID: 1
+  }
+} */
+
+
+
+
+
+function ProjectCard(props) {
+  
+  return (
+    <Card>
+      <Heading size="lg" color="teal.400" align="center" py={{ base: 4, md: 0 }} w="50%">
+        {props.project.projectName}
+      </Heading>
+      <Spacer />
+      <HStack p={2} spacing={0}>
+        <Text fontWeight="bolder" fontSize="md">
+          {prod1mock.area}
+        </Text>
+        <Text fontSize="sm" color="gray.400">
+          Product
+        </Text>
+      </HStack>
+      <Spacer />
+      <Button bg="teal.500" align="center" _hover={{ bg: 'teal.400' }} w={24}>
+        Edit
+      </Button>
+    </Card>
+  );
+}
+
+function TextF(props){
+return(
+  <p>{props.project}</p> 
+  //<p>{proj}</p> 
+)
+}
 
 export default function ProductOverview() {
+  const [projectsData, setProjectsData] = useState([project1])
+
   let [value, setValue] = React.useState("")
 
   let handleInputChange = (e) => {
@@ -15,64 +97,23 @@ export default function ProductOverview() {
   return (
     <div>
       <Page title="Product Overview">
-        <Tabs>
-          <TabList>
-            <Tab>Kredit</Tab>
-            <Tab>Payments</Tab>
-            <Tab>Customer</Tab>
-          </TabList>
-
-          <TabPanels>
-            <TabPanel>
-            <Wrap>
-              <WrapItem>
-                <Center>
-                
-              
-                  <SimpleGrid columns={5} spacing={10}>
-                    <Box bg="teal.300" color="black" borderRadius="md" textAlign="Center">Produkte</Box>
-                    <Box bg="teal.300" color="black" borderRadius="md" textAlign="Center">Wirtschaftliche Bewertung</Box>
-                    <Box bg="teal.300" color="black" borderRadius="md" textAlign="Center">Komplexitäts- bewertung</Box>
-                    <Box bg="teal.300" color="black" borderRadius="md" textAlign="Center">Anmerkung</Box>
-                    <Box bg="teal.300" color="black" borderRadius="md" textAlign="Center">Nachweis </Box>
-                  </SimpleGrid>
-              </Center>
-            </WrapItem>
-            <WrapItem>
-              <Center>
-              <SimpleGrid columns={5} spacing={10}>
-
-                <Box bg="gray.300" color="black" borderRadius="md" textAlign="Center">Dispokredit</Box>
-                <Box borderRadius="md" textAlign="Center">
-                <IconButton colorScheme="blue" aria-label="Search database"  />
-                  <CircularProgress value={40} color="green.400">
-                    <CircularProgressLabel>40%</CircularProgressLabel>
-                  </CircularProgress></Box>
-                <Box bg="gray.300" color="black" borderRadius="md" textAlign="Center">Komplexitäts- bewertung</Box>
-                <Box bg="gray.300" color="black" borderRadius="md" textAlign="Center">Anmerkung</Box>
-                <Box borderRadius="md" textAlign="Center">
-                  <><Text mb="8px">Value: {value}</Text>
-                    <Textarea value={value} onChange={handleInputChange} placeholder="Here is a sample placeholder" size="sm"/>
-                  </>
-                </Box>
-                
-              </SimpleGrid>
-              </Center>
-            </WrapItem>
-              </Wrap>
-              <Button size="lg">
-        Add new
-      </Button>
-            </TabPanel>
-            <TabPanel>
-              <p>two!</p>
-            </TabPanel>
-            <TabPanel>
-              <p>three!</p>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
+      <List spacing={3} maxW="900px" mx={2}>
+      {projectsData.map((project) => (
+          //<ProjectCard project={project} key={project.projectID}></ProjectCard>
+          <TextF project={project.project.projectName}/>
+          
+        ))}
+      </List>
+      <Box> {projectsData.project}</Box>
       </Page>
     </div>
   )
 }
+/* 
+const project1 = {
+  project: {
+    projectName: "TestFName1",
+    projectID: 0,
+  }
+}
+*/
