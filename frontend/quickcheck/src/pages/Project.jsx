28be543ec94b49e9e4af6a34a2eeca5b4f3ad8bf
Tobi B <to-tb@gmx.de>
2021-11-29 16:36:50 +0100

    clean up project page

--- frontend/quickcheck/src/pages/Project.jsx
+++ frontend/quickcheck/src/pages/Project.jsx
@@ -28,73 +28,6 @@ import { api } from '../utils/apiClient';
 import Card from '../components/Card';
 import { CloseIcon, EditIcon, CheckIcon } from '@chakra-ui/icons';
 
-function MemberTable({ members }) {
-  return (
-    <Table w="full" size="md" bg="gray.600" rounded="md">
-      <Thead size='2xl'>
-        <Tr>
-          <Th w="40%" >ID</Th>
-          <Th w="40%">Name</Th>
-          <Th w="20%" isNumeric>
-            btn
-          </Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {members.map((id) => (
-          <Tr>
-            <Td>{id}</Td>
-            <Td>{id}</Td>
-            <Td isNumeric>
-              <Button bg="red" size="sm" m={0} />
-              <Button bg="red" size="sm" m={0} />
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-      <Tfoot></Tfoot>
-    </Table>
-  );
-}
-
-function CustomEditable({ value, valueSetter, showEditButtons }) {
-  const [stagedValue, setStagedValue] = useState('hello');
-  const [editMode, setEditMode] = useState(false);
-
-  const contentEdit = () => {
-    return (
-      <Input
-        value={stagedValue}
-        onChange={(e) => {
-          setStagedValue(e.target.value);
-        }}
-      ></Input>
-    );
-  };
-
-  const contentView = () => {
-    return <Text>value{stagedValue}</Text>;
-  };
-
-  if (editMode) {
-    return (
-      <>
-        <contentEdit />
-        <IconButton size="sm" icon={<CheckIcon />} onClick={(e) => setEditMode(false)} />
-        <IconButton size="sm" icon={<CloseIcon />} onClick={(e) => setEditMode(false)} />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <contentView />
-        <IconButton size="sm" icon={<EditIcon />} onClick={(e) => setEditMode(true)} />
-      </>
-    );
-  }
-  return <Button>{value}</Button>;
-}
-
 
 export default function Project() {
   const [projectData, setprojectData] = useState({
@@ -127,8 +60,6 @@ export default function Project() {
     });
   };
 
-  const [editMode, setEditMode] = useState(false);
-  const [testText, setTestText] = useState('hello');
 
   return (
     <Page title="Project">
@@ -139,7 +70,6 @@ export default function Project() {
 
       <Card direction="column">
         <Heading size="lg">Project Members</Heading>
-        <MemberTable members={projectData.members} />
       </Card>
 
       <Card>
@@ -147,10 +77,6 @@ export default function Project() {
         <Button onClick={() => setHeader('clicked')}>hello</Button>
       </Card>
 
-
-      <CustomEditable value={testText} valueSetter={setTestText} showEditButtons={true} />
-
-      <Input value={'hello'}></Input>
     </Page>
   );
 }
