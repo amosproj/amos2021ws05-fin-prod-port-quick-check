fc19044f37485e312fd2bc10b37972a097e5e535
Tobi B <to-tb@gmx.de>
2021-12-04 03:32:20 +0100

    Add ConditionalInput

--- frontend/quickcheck/src/components/Inputs.jsx
+++ frontend/quickcheck/src/components/Inputs.jsx
@@ -1,14 +1,29 @@
-import { Select } from '@chakra-ui/select';
+import { Select, Input, Text, Box } from '@chakra-ui/react';
 
 export function Selection(props) {
   // required args: options, selected (option), onChange :updateFunction
   return (
-    <Select {...props} onChange={(e) => props.onChange(e.target.value)}>
+    <Select {...props}>
       {props.options.map((opt) => (
-        <option selected={opt === props.selected} value={opt}>
+        <option selected={opt === props.selected} value={opt} key={opt}>
           {opt}
         </option>
       ))}
     </Select>
+  );
+}
+
+// pass value and onchange just like with a regular input
+export function ConditionalInput(props) {
+  return (
+    <Box align="center" {...props} rounded="md">
+      {props.editable ? (
+        <Input value={props.value} onChange={props.onChange} {...props.fontStyle} />
+      ) : (
+        <Text align="left" px={4} py={2} {...props.fontStyle}>
+          {props.value}
+        </Text>
+      )}
+    </Box>
   );
 }
