b850850006be538890500ad4fe489d285e6e48d3
Ines <ine.vogel@googlemail.com>
2021-12-04 18:49:17 +0100

    uncomments functionality for ProductRating

--- service/src/main/java/com/tu/FinancialQuickCheck/db/ProductRatingEntity.java
+++ service/src/main/java/com/tu/FinancialQuickCheck/db/ProductRatingEntity.java
@@ -1,25 +1,25 @@
-//package com.tu.FinancialQuickCheck.db;
-//
-//import com.tu.FinancialQuickCheck.Score;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//public class ProductRatingEntity {
-//    // TODO: überlegen wie Dateien gespeicht werden, und ob hier ein Verweis notwendig ist
-//
-//    @EmbeddedId
-//    public ProductRatingId productRatingId;
-//
-//    @Column(name = "answer")
-//    public String answer;
-//
-//    // TODO: wie gehen wir mit anderen Scalenwerten um?
-//    @Column(name = "score")
-//    public Score score;
-//
-//    @Column(name = "comment")
-//    public String comment;
-//
-//    public ProductRatingEntity(){}
-//}
+package com.tu.FinancialQuickCheck.db;
+
+import com.tu.FinancialQuickCheck.Score;
+
+import javax.persistence.*;
+
+@Entity
+public class ProductRatingEntity {
+    // TODO: überlegen wie Dateien gespeicht werden, und ob hier ein Verweis notwendig ist
+
+    @EmbeddedId
+    public ProductRatingId productRatingId;
+
+    @Column(name = "answer")
+    public String answer;
+
+    // TODO: wie gehen wir mit anderen Scalenwerten um?
+    @Column(name = "score")
+    public Score score;
+
+    @Column(name = "comment")
+    public String comment;
+
+    public ProductRatingEntity(){}
+}
