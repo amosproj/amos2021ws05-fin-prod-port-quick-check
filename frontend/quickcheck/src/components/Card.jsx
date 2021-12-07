d2d10ada0f35d16f05dccdb40b3dd5e8a6597d20
Tobi B <to-tb@gmx.de>
2021-12-07 17:24:30 +0100

    add some documentation to card componant

--- frontend/quickcheck/src/components/Card.jsx
+++ frontend/quickcheck/src/components/Card.jsx
@@ -5,6 +5,9 @@ export default function Card({ layerStyle, children, ...rest }) {
   const bg = useColorModeValue('gray.50', 'gray.700');
   const barColor = useColorModeValue('primary', 'secondary');
 
+  // use property gridGap={some_int} with have gap between children
+  // use justifyContent property to align items left, right, center, spaced out, spaced evenly
+  //      (check out https://medium.com/@lainakarosic/getting-started-with-css-flexbox-basics-58d875a574ce)
   return (
     <Flex
       {...rest}
