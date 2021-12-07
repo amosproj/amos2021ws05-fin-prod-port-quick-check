4b60f776a6ac16e890ff13e66ea854327339ed3b
tobbber <tobbber@users.noreply.github.com>
2021-12-07 05:48:58 +0000

    Prettified Code!

--- frontend/quickcheck/src/pages/TestRange.jsx
+++ frontend/quickcheck/src/pages/TestRange.jsx
@@ -16,77 +16,74 @@ export default function TestRange() {
 
   return (
     <>
-    <Page>
-      <Box
-        align="center"
-        w="80%"
-        h="80%"
-        rounded="2xl"
-        bg="bg.300"
-        border="10px"
-        borderWidth="10px"
-        borderColor="red"
-      >
-        <Button rounded="3xl" m={3} colorScheme="brand">
-          button uses custom color, colorScheme is automatic but only works for buttons
-        </Button>
-        <Heading color={headingColor}>switching color for dark and bright</Heading>
-        <Box border="1px" borderColor="blue">
-          Card
+      <Page>
+        <Box
+          align="center"
+          w="80%"
+          h="80%"
+          rounded="2xl"
+          bg="bg.300"
+          border="10px"
+          borderWidth="10px"
+          borderColor="red"
+        >
+          <Button rounded="3xl" m={3} colorScheme="brand">
+            button uses custom color, colorScheme is automatic but only works for buttons
+          </Button>
+          <Heading color={headingColor}>switching color for dark and bright</Heading>
+          <Box border="1px" borderColor="blue">
+            Card
+          </Box>
         </Box>
-      </Box>
-      <Card variant="brand"></Card>
+        <Card variant="brand"></Card>
 
-      <Flex variant="brand" p="2">
-        <Button size="lg" variant="primary">
-          click me
-        </Button>
-        <Button shadow="outline" size="lg" variant="primary">
-          click me
-        </Button>
-        <Button isLoading size="lg" variant="primary">
-          click me
-        </Button>
-        <Button disabled size="lg" variant="primary">
-          click me
-        </Button>
-      </Flex>
+        <Flex variant="brand" p="2">
+          <Button size="lg" variant="primary">
+            click me
+          </Button>
+          <Button shadow="outline" size="lg" variant="primary">
+            click me
+          </Button>
+          <Button isLoading size="lg" variant="primary">
+            click me
+          </Button>
+          <Button disabled size="lg" variant="primary">
+            click me
+          </Button>
+        </Flex>
 
-      <Flex variant="brand" p="2">
-        <Button size="lg" variant="secondary">
-          click me
-        </Button>
-        <Button shadow="outline" size="lg" variant="secondary">
-          click me
-        </Button>
-        <Button isLoading size="lg" variant="secondary">
-          click me
-        </Button>
-        <Button disabled size="lg" variant="secondary">
-          click me
-        </Button>
-      </Flex>
+        <Flex variant="brand" p="2">
+          <Button size="lg" variant="secondary">
+            click me
+          </Button>
+          <Button shadow="outline" size="lg" variant="secondary">
+            click me
+          </Button>
+          <Button isLoading size="lg" variant="secondary">
+            click me
+          </Button>
+          <Button disabled size="lg" variant="secondary">
+            click me
+          </Button>
+        </Flex>
 
-      <Flex variant="brand" p="2">
-        <Button size="lg" variant="wisper">
-          click me
-        </Button>
-        <Button shadow="outline" focus size="lg" variant="wisper">
-          click me
-        </Button>
-        <Button isLoading size="lg" variant="wisper">
-          click me
-        </Button>
-        <Button disabled size="lg" variant="wisper">
-          click me
-        </Button>
-      </Flex>
-
-
-    </Page>
-    <Box  bg='red' w='full' h={3} roundedTop='lg' m='0'></Box>
-<Flex bg='blue'  w='full' h={20} roundedBottom='lg'  m='0'  align='center'
-></Flex>
-          </>
+        <Flex variant="brand" p="2">
+          <Button size="lg" variant="wisper">
+            click me
+          </Button>
+          <Button shadow="outline" focus size="lg" variant="wisper">
+            click me
+          </Button>
+          <Button isLoading size="lg" variant="wisper">
+            click me
+          </Button>
+          <Button disabled size="lg" variant="wisper">
+            click me
+          </Button>
+        </Flex>
+      </Page>
+      <Box bg="red" w="full" h={3} roundedTop="lg" m="0"></Box>
+      <Flex bg="blue" w="full" h={20} roundedBottom="lg" m="0" align="center"></Flex>
+    </>
   );
 }
