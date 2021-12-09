58761d8c841342425572a3fe3a8402cbf06e1933
Tobi B <to-tb@gmx.de>
2021-12-09 14:53:06 +0100

    implement state management in project overview

--- frontend/quickcheck/src/store.js
+++ frontend/quickcheck/src/store.js
@@ -5,25 +5,55 @@ import { api } from './utils/apiClient';
 // Documentation for easy-peasy: https://easy-peasy.vercel.app/docs/tutorials/quick-start.html
 
 // first define the store model, then add actions to change the stored state and add 'thunks' for actions with side effects (e.g. api calls)
-const store = createStore({
-  project: {
+
+const projectListModel = {
+  items: [], // list of: {"projectID": 2,"projectName": "Mock Project" }
+
+  set: action((state, items) => {state.items = items}),
+  add: action((state, newProject) => {state.items.push(newProject)}),
+  
+  fetch: thunk(async (actions, payload) => {
+    await api
+      .url('/projects')
+      .get()
+      .json((json) => actions.set(json))
+      .catch(console.error);
+  }),
+}
+
+const projectModel = {
+  data: {
     projectID: 0,
     projectName: '',
     members: [],
     productAreas: [],
   },
-
-  updateProject: action((state, payload) => {
-    state.project = { ...state.project, ...payload }; // to change name pass {name: 'new name' } as payload
-  }),
-
-  fetchProject: thunk(async (actions, payload) => {
+  // to change name pass {name: 'new name' } as payload
+  update: action((state, updatedProps) => {state.project = { ...state.project, ...updatedProps }}),
+  // GET project by id
+  fetch: thunk(async (actions, id) => {
     await api
-      .url('/projects/' + payload.projectID)
+      .url('/projects/' + id)
       .get()
       .json((json) => actions.updateProject(json))
       .catch(console.error);
   }),
+
+  // POST new Project 
+  create: thunk(async (actions, newProject) => {
+    console.log(newProject);
+    await api
+      .url('/projects')
+      .post(newProject)
+      .res(console.log)
+  }),
+}
+
+
+
+const store = createStore({ 
+  projectList: projectListModel,
+  project: projectModel,
 });
 
 export default store;
