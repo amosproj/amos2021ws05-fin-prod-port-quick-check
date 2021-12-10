1598e213b152dec8ddc90eef7f7cf97f4e68a3a3
tobbber <tobbber@users.noreply.github.com>
2021-12-10 20:47:09 +0000

    Prettified Code!

--- frontend/quickcheck/src/pages/projectPage/Page.jsx
+++ frontend/quickcheck/src/pages/projectPage/Page.jsx
@@ -74,9 +74,9 @@ export default function Project(prop) {
           Project:
         </Heading>
         <Input
-          variant='bold'
-          size='3xl'
-          w='auto'
+          variant="bold"
+          size="3xl"
+          w="auto"
           isDisabled={!editMode}
           onChange={(e) => handleChange('projectName')(e.target.value)}
           value={projectData.projectName}
