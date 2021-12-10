301b5b58ea7a243c6395079e3e47cab5f4a71e38
InesVogel <InesVogel@users.noreply.github.com>
2021-12-10 07:57:36 +0000

    Prettified Code!

--- frontend/quickcheck/src/utils/const.js
+++ frontend/quickcheck/src/utils/const.js
@@ -1,8 +1,7 @@
-
 export const roles = {
-    consultant: "Consultant",
-    manager: "Project Manager",
-    owner: "Project Owner"
-}
+  consultant: 'Consultant',
+  manager: 'Project Manager',
+  owner: 'Project Owner',
+};
 
 // todo use this definition instread of hard coded strings for bsp if(role === "Consultant") ...
