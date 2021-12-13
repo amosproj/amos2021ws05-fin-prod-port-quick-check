4c65ce53c33a9271c67f90f2b2b0c8401f18adc6
tobbber <tobbber@users.noreply.github.com>
2021-12-13 17:20:48 +0000

    Prettified Code!

--- frontend/quickcheck/src/pages/projectPage/MemberTable.jsx
+++ frontend/quickcheck/src/pages/projectPage/MemberTable.jsx
@@ -57,8 +57,8 @@ export default function MemberTable({ editMode, members, handleChange }) {
   const handleRoleChange = (member) => (newRole) => {
     // This is a curried function in JS
     let index = members.map((m) => m.email).indexOf(member.email);
-    member.role = newRole.target.value//{ ...member, role: newRole };
-    members[index] = member
+    member.role = newRole.target.value; //{ ...member, role: newRole };
+    members[index] = member;
     handleChange(members);
   };
 
