0dd04e25ef9c7d683a2a42b77ff67ae29bd6dccb
aron4 <aron4@users.noreply.github.com>
2021-12-13 17:56:22 +0000

    Prettified Code!

--- frontend/quickcheck/src/pages/ProductOverview.jsx
+++ frontend/quickcheck/src/pages/ProductOverview.jsx
@@ -143,7 +143,6 @@ export default function ProductOverview() {
   const [productsData, setProductsData] = useState(products);
   const [editMode, setEditMode] = useState(false);
 
-
   const set = () => {
     setProducts(products);
   };
@@ -176,11 +175,13 @@ export default function ProductOverview() {
     }
   };
   const addProduct = (productName) => {
-    const prod = {productID: 0,
+    const prod = {
+      productID: 0,
       productName: productName,
       productArea: {},
       projectID: uuid4(),
-      parentID: uuid4}
+      parentID: uuid4,
+    };
     addProductAction(prod);
   };
 
