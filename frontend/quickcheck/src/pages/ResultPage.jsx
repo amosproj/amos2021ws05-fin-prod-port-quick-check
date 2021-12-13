7dde161ac5f194329a5e9f38eee4a0403f9c2bab
Andreas Ronellenfitsch <bn0122@partner.kit.edu>
2021-12-13 21:32:09 +0100

    Initialise result page

---
+++ frontend/quickcheck/src/pages/ResultPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from '../components/Card';
+import Page from '../components/Page';
+
+export default function ResultPage() {
+    return (
+        <div>
+             <Card layerStyle="card_bordered"></Card>
+        </div>
+    )
+}
