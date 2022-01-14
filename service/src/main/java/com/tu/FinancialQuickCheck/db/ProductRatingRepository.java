8781ad982c50b668bc819df31dd059debfd782fe
Ines <ine.vogel@googlemail.com>
2022-01-14 19:33:17 +0100

    adds TODOs to ProductRatingRepository

--- service/src/main/java/com/tu/FinancialQuickCheck/db/ProductRatingRepository.java
+++ service/src/main/java/com/tu/FinancialQuickCheck/db/ProductRatingRepository.java
@@ -12,7 +12,9 @@ import java.util.List;
  */
 public interface ProductRatingRepository extends JpaRepository<ProductRatingEntity, ProductRatingId> {
 
-    // TODO: was wird zurück gegeben wenn projectID nicht existiert? leere Liste(?)
+    // TODO: was wird zurück gegeben wenn projectID nicht existiert? --> leere Liste
+    // TODO: what is returned of ratings do not exist but ids exist?
+
     @Query("SELECT distinct pr from ProductRatingEntity as pr " +
             "join ProductEntity as p on pr.productRatingId.product = p " +
             "join RatingEntity r on r = pr.productRatingId.rating " +
@@ -21,12 +23,14 @@ public interface ProductRatingRepository extends JpaRepository<ProductRatingEnti
             "or pr.productRatingId.rating.id = 9)")
     List<ProductRatingEntity> getResultsByProject(@Param("projectId") int projectId);
 
-    // TODO: was wird zurück gegeben wenn projectID nicht existiert? leere Liste(?)
+
     @Query("SELECT distinct pr from ProductRatingEntity as pr " +
             "join ProductEntity as p on pr.productRatingId.product = p " +
             "join RatingEntity r on r = pr.productRatingId.rating " +
             "where pr.productRatingId.product.project.id = :projectId " +
-            "and pr.productRatingId.product.productarea.id = :productAreaId and pr.productRatingId.rating.id = 4")
+            "and pr.productRatingId.product.productarea.id = :productAreaId " +
+            "and (pr.productRatingId.rating.id = 4 or pr.productRatingId.rating.id = 5 " +
+            "or pr.productRatingId.rating.id = 10 or pr.productRatingId.rating.id = 9)")
     List<ProductRatingEntity> getResultsByProjectAndProductArea(@Param("projectId") int projectId,
                                                                 @Param("productAreaId") int productAreaId);
 
