12de1dbbd52f2dc7bb0c3dba55724adf78daf1b7
Ines <ine.vogel@googlemail.com>
2022-01-14 18:56:43 +0100

    refactors ResultDto and ScoreDto

--- service/src/main/java/com/tu/FinancialQuickCheck/dto/ResultDto.java
+++ service/src/main/java/com/tu/FinancialQuickCheck/dto/ResultDto.java
@@ -1,6 +1,9 @@
 package com.tu.FinancialQuickCheck.dto;
 
+import com.tu.FinancialQuickCheck.Score;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -9,16 +12,29 @@ import java.util.List;
  */
 public class ResultDto {
 
+    public int productID;
     public String productName;
     public List<ProductRatingDto> ratings;
-    public List<ScoreDto> scores;
+    public ScoreDto[] scores;
 
-    public ResultDto(){}
+    public ResultDto(){
+        this.ratings = new ArrayList<>();
+        this.scores = new ScoreDto[3];
+        for(int i = 0; i < scores.length; i++){
+            scores[i] = new ScoreDto(Score.valueOf(i + 1), 0);
+        }
+    }
 
-    public ResultDto(String productName, List<ProductRatingDto> ratings, List<ScoreDto> scores)
+    public ResultDto(int productID, String productName, List<ProductRatingDto> ratings, ScoreDto[] scores)
     {
+        this.productID = productID;
         this.productName = productName;
         this.ratings = ratings;
         this.scores = scores;
+    }
+
+    public void updateProductInfos(int productID, String productName){
+        this.productID = productID;
+        this.productName = productName;
     }
 }
--- service/src/main/java/com/tu/FinancialQuickCheck/dto/ScoreDto.java
+++ service/src/main/java/com/tu/FinancialQuickCheck/dto/ScoreDto.java
@@ -2,6 +2,8 @@ package com.tu.FinancialQuickCheck.dto;
 
 import com.tu.FinancialQuickCheck.Score;
 
+import java.util.Objects;
+
 /**
  *
  *
@@ -17,5 +19,18 @@ public class ScoreDto {
     {
         this.score = score;
         this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScoreDto scoreDto = (ScoreDto) o;
+        return score == scoreDto.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
     }
 }
