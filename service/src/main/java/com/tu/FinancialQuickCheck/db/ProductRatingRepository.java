a9e54b52c39929442f54117e48a299dc856e3552
Ines <ine.vogel@googlemail.com>
2022-01-14 18:55:57 +0100

    adds sql queries for ResultService

--- service/src/main/java/com/tu/FinancialQuickCheck/db/ProductRatingRepository.java
+++ service/src/main/java/com/tu/FinancialQuickCheck/db/ProductRatingRepository.java
@@ -1,11 +1,33 @@
 package com.tu.FinancialQuickCheck.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * JPA repository infrastructure scans classpath for ProductRatingRepository interface, creates a Spring bean for it and
  * implements CRUD-methods.
  */
 public interface ProductRatingRepository extends JpaRepository<ProductRatingEntity, ProductRatingId> {
+
+    // TODO: was wird zurück gegeben wenn projectID nicht existiert? leere Liste(?)
+    @Query("SELECT distinct pr from ProductRatingEntity as pr " +
+            "join ProductEntity as p on pr.productRatingId.product = p " +
+            "join RatingEntity r on r = pr.productRatingId.rating " +
+            "where pr.productRatingId.product.project.id = :projectId and (pr.productRatingId.rating.id = 4 " +
+            "or pr.productRatingId.rating.id = 5 or pr.productRatingId.rating.id = 10 " +
+            "or pr.productRatingId.rating.id = 9)")
+    List<ProductRatingEntity> getResultsByProject(@Param("projectId") int projectId);
+
+    // TODO: was wird zurück gegeben wenn projectID nicht existiert? leere Liste(?)
+    @Query("SELECT distinct pr from ProductRatingEntity as pr " +
+            "join ProductEntity as p on pr.productRatingId.product = p " +
+            "join RatingEntity r on r = pr.productRatingId.rating " +
+            "where pr.productRatingId.product.project.id = :projectId " +
+            "and pr.productRatingId.product.productarea.id = :productAreaId and pr.productRatingId.rating.id = 4")
+    List<ProductRatingEntity> getResultsByProjectAndProductArea(@Param("projectId") int projectId,
+                                                                @Param("productAreaId") int productAreaId);
 
 }
--- service/src/main/java/com/tu/FinancialQuickCheck/db/ProductRepository.java
+++ service/src/main/java/com/tu/FinancialQuickCheck/db/ProductRepository.java
@@ -22,19 +22,4 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Integer>
      */
     Iterable<ProductEntity> findByProjectAndProductarea(ProjectEntity project, ProductAreaEntity productArea);
 
-    // TODO: was wird zurück gegeben wenn projectID nicht existiert? leere Liste(?)
-    @Query("SELECT p from ProductEntity as p " +
-            "join ProductRatingEntity as pr on p = pr.productRatingId.product " +
-            "join RatingEntity r on r = pr.productRatingId.rating " +
-            "where p.project.id = :projectId and (r.id = 4 or r.id = 5 or r.id = 9 or r.id = 10)")
-    List<ProductEntity> getResultsByProject(@Param("projectId") int projectId);
-
-    // TODO: was wird zurück gegeben wenn projectID und/oder productAreaID nicht existiert? leere Liste(?)
-    @Query("SELECT p from ProductEntity as p " +
-            "join ProductRatingEntity as pr on p = pr.productRatingId.product " +
-            "join RatingEntity r on r = pr.productRatingId.rating " +
-            "where p.project.id = :projectId and p.productarea.id = :productAreaId " +
-            "and (r.id = 4 or r.id = 5 or r.id = 9 or r.id = 10)")
-    List<ProductEntity> getResultsByProjectAndProductArea(@Param("projectId") int projectId,
-                                                           @Param("productAreaId") int productAreaId);
 }
