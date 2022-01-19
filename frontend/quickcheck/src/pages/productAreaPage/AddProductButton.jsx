d1f56a0f6e661a0b0e255430cbaf60da42c6de34
tobbber <tobbber@users.noreply.github.com>
2022-01-19 15:59:56 +0000

    Prettified Code!

--- frontend/quickcheck/src/pages/productAreaPage/AddProductButton.jsx
+++ frontend/quickcheck/src/pages/productAreaPage/AddProductButton.jsx
@@ -20,45 +20,44 @@ import {
 } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
 
-
 export default function AddProductButton({ onAddProduct }) {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [productName, setProductName] = useState('');
-    const header = 'Add Product';
-    return (
-      <>
-        <IconButton icon={<AddIcon />} variant="primary" size="lg" onClick={onOpen} />
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader color="primary">{header}</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody px={10}>
-              <FormControl>
-                <Input
-                  mb={6}
-                  placeholder="Product Name"
-                  onChange={(e) => setProductName(e.target.value)}
-                />
-              </FormControl>
-            </ModalBody>
-            <ModalFooter py={5} px={10}>
-              <Button
-                variant="primary"
-                mx={3}
-                onClick={(e) => {
-                  onAddProduct(productName);
-                  onClose();
-                }}
-              >
-                Save
-              </Button>
-              <Button onClick={onClose} variant="wisper">
-                Cancel
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    );
-  }
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [productName, setProductName] = useState('');
+  const header = 'Add Product';
+  return (
+    <>
+      <IconButton icon={<AddIcon />} variant="primary" size="lg" onClick={onOpen} />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader color="primary">{header}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody px={10}>
+            <FormControl>
+              <Input
+                mb={6}
+                placeholder="Product Name"
+                onChange={(e) => setProductName(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter py={5} px={10}>
+            <Button
+              variant="primary"
+              mx={3}
+              onClick={(e) => {
+                onAddProduct(productName);
+                onClose();
+              }}
+            >
+              Save
+            </Button>
+            <Button onClick={onClose} variant="wisper">
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
