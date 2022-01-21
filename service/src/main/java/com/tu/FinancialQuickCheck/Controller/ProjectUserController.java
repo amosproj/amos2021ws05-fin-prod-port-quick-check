cf187918523b643667e177e7b43de152500e82a9
Ines <ine.vogel@googlemail.com>
2022-01-21 16:02:29 +0100

    adds cors configuration (only allows requests from frontend)

--- service/src/main/java/com/tu/FinancialQuickCheck/Controller/ProjectUserController.java
+++ service/src/main/java/com/tu/FinancialQuickCheck/Controller/ProjectUserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/projects/{projectID}/users")
 public class ProjectUserController {
