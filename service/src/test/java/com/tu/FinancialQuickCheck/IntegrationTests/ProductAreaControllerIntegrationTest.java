42d960d14769415edfb249aa693fba05b2fbf273
Ines <ine.vogel@googlemail.com>
2022-02-01 20:16:41 +0100

    refactors product area controller tests

--- service/src/test/java/com/tu/FinancialQuickCheck/IntegrationTests/ProductAreaControllerIntegrationTest.java
+++ service/src/test/java/com/tu/FinancialQuickCheck/IntegrationTests/ProductAreaControllerIntegrationTest.java
@@ -6,7 +6,6 @@ import com.tu.FinancialQuickCheck.db.ProductAreaEntity;
 import com.tu.FinancialQuickCheck.db.ProductAreaRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,12 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
-import java.util.List;
 import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The current test class verifies the functionalities of the Product Area Controller
@@ -38,15 +35,15 @@ public class ProductAreaControllerIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-
     private String host = "http://localhost:";
     private String productAreas = "/productareas";
 
-    HttpHeaders header = new HttpHeaders();
-    String jsonStringCorrect = "{\"category\":\"PRIVAT\", \"name\":\"KREDIT\"}";
-    String jsonStringEmpty = "{}";
-    String jsonStringMissingCategory = "{\"name\":\"KREDIT\"}";
-    String jsonStringMissingName = "{\"category\":\"PRIVAT\"}";
+    private HttpHeaders header = new HttpHeaders();
+    private String jsonStringCorrect = "{\"category\":\"PRIVAT\", \"name\":\"KREDIT\"}";
+    private String jsonStringEmpty = "{}";
+    private String jsonStringMissingCategory = "{\"name\":\"KREDIT\"}";
+    private String jsonStringMissingName = "{\"category\":\"PRIVAT\"}";
+
     private ProductAreaEntity entity;
 
     @BeforeEach
@@ -55,112 +52,79 @@ public class ProductAreaControllerIntegrationTest {
 
         header.setContentType(MediaType.APPLICATION_JSON);
 
-        ProductAreaEntity tmp = new ProductAreaEntity();
-        tmp.category = "PRIVAT";
-        tmp.name = "KREDIT";
-        repository.save(tmp);
-        List<ProductAreaEntity> out = repository.findAll();
-
         entity = new ProductAreaEntity();
-        entity.id = out.get(0).id;
-        entity.name = out.get(0).name;
-        entity.category = out.get(0).category;
+        entity.category = "PRIVAT";
+        entity.name = "KREDIT";
+        repository.save(entity);
     }
 
     @AfterEach
     public void reset(){
-        repository.delete(entity);
+        repository.deleteAll();
     }
 
     @Test
-    public void test1_getProductAreas_expectResourceNotFound() {
+    public void test1_getProductAreas_resourceNotFound() {
         // delete all existing productAreas
         repository.deleteAll();
 
-        // test object
-        ResponseEntity<String> response = restTemplate.exchange(
-                host + port + productAreas,
-                HttpMethod.GET,
-                null,
-                String.class);
+        ResponseEntity<String> response = restTemplate.exchange(host + port + productAreas,
+                HttpMethod.GET,null, String.class);
 
-        log.info("@Test 1 - getProductAreas_expectResourceNotFound");
-        log.info("@Test 1 - Response Status: " + response.getStatusCode());
-        log.info("@Test 1 - Response Status Code Value: " + response.getStatusCodeValue());
+        log.info("@Test 1 - getProductAreas_resourceNotFound - no product areas in db");
         log.info("@Test 1 - Response Body: " + response.getBody());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
     @Test
-    public void test2_getProductAreas_expectSuccess_200() {
-        ResponseEntity<String> response = restTemplate.exchange(
-                host + port + productAreas,
-                HttpMethod.GET,
-                null,
-                String.class);
+    public void test2_getProductAreas_success_200(){
+        ResponseEntity<String> response = restTemplate.exchange(host + port + productAreas,
+                HttpMethod.GET,null, String.class);
 
-        log.info("@Test 2 - getProductAreas_expectSuccess_200");
-        log.info("@Test 2 - Response Status: " + response.getStatusCode());
-        log.info("@Test 2 - Response Status Code Value: " + response.getStatusCodeValue());
+        log.info("@Test 2 - getProductAreas_success_200");
         log.info("@Test 2 - Response Body: " + response.getBody());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
-    public void test3_postProductAreas_expectCreated_201() throws JsonProcessingException {
+    public void test3_postProductAreas_created_201() throws JsonProcessingException {
         HttpEntity<String> request = new HttpEntity<>(jsonStringCorrect, header);
 
-        ResponseEntity<String> response = restTemplate.exchange(
-                host + port + productAreas,
-                HttpMethod.POST,
-                request,
-                String.class);
+        ResponseEntity<String> response = restTemplate.exchange(host + port + productAreas,
+                HttpMethod.POST, request, String.class);
 
-        log.info("@Test 3 - postProductAreas_expectCreated_201");
-        log.info("@Test 3 - Response Status: " + response.getStatusCode());
-        log.info("@Test 3 - Response Status Code Value: " + response.getStatusCodeValue());
+        log.info("@Test 3 - postProductAreas_created_201");
         log.info("@Test 3 - Response Body: " + response.getBody());
 
-        ProductAreaEntity productArea = new ObjectMapper().readValue(response.getBody(), ProductAreaEntity.class);
-        assertThat(productArea.id).isGreaterThan(0);
-        assertEquals("KREDIT", productArea.name);
-        assertEquals("PRIVAT", productArea.category);
+        ProductAreaEntity out = new ObjectMapper().readValue(response.getBody(), ProductAreaEntity.class);
+        assertEquals(entity.id + 1, out.id);
+        assertEquals("KREDIT", out.name);
+        assertEquals("PRIVAT", out.category);
+        assertNull(out.productEntities);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
 
     @Test
-    public void test4_postProductAreas_expectBadRequest(){
+    public void test4_postProductAreas_badRequest(){
         String[] testObjects = new String[]{"", jsonStringEmpty, jsonStringMissingName, jsonStringMissingCategory};
         for (String jsonString: testObjects) {
-            HttpEntity<String> request = new HttpEntity<>(jsonString, header);
-
-            ResponseEntity<String> response = restTemplate.exchange(
-                    host + port + productAreas,
-                    HttpMethod.POST,
-                    request,
-                    String.class);
+            ResponseEntity<String> response = restTemplate.exchange(host + port + productAreas,
+                    HttpMethod.POST, new HttpEntity<>(jsonString, header), String.class);
 
             log.info("@Test 4 - postProductAreas_expectBadRequest");
             log.info("@Test 4 - Request Body: " + jsonString);
-            log.info("@Test 4 - Response Status: " + response.getStatusCode());
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         }
     }
 
     @Test
     public void test5_notAllowedMethodsProductAreas() {
-
         HttpMethod[] notAllowed = new HttpMethod[]{HttpMethod.PUT, HttpMethod.DELETE};
         for (HttpMethod httpMethod : notAllowed) {
-            ResponseEntity<String> response = restTemplate.exchange(
-                    host + port + productAreas,
-                    httpMethod,
-                    null,
-                    String.class);
+            ResponseEntity<String> response = restTemplate.exchange(host + port + productAreas,
+                    httpMethod, null, String.class);
 
             log.info("@Test 5 - notAllowedMethodsProductAreas  - method: " + httpMethod);
-            log.info("@Test 5 - Response Status: " + response.getStatusCode());
-            log.info("@Test 5 - Response Status Code Value: " + response.getStatusCodeValue());
             log.info("@Test 5 - Response Body: " + response.getBody());
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.METHOD_NOT_ALLOWED);
         }
