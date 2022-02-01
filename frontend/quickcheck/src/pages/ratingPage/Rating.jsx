f367ecfc4e56bea76ce5e848ada21fad41129172
Andreas Ronellenfitsch <bn0122@partner.kit.edu>
2022-02-01 20:43:56 +0100

    Remove evaluation button

--- frontend/quickcheck/src/pages/ratingPage/Rating.jsx
+++ frontend/quickcheck/src/pages/ratingPage/Rating.jsx
@@ -111,9 +111,6 @@ export default function Rating() {
           >
             Save
           </Button>
-          <Link href={`/projects/${projectID}/productArea/${productAreaID}/products/${productID}/evaluation`}>
-            <Button variant={"whisper"}>Evaluation</Button>
-          </Link>
           <Link href={`/projects/${projectID}/productArea/${productAreaID}/`}>
             <Button variant="whisper">Back</Button>
           </Link>
