0c6b26fd680e57d25f7aa3675e7e1843a412ceb2
Ines <ine.vogel@googlemail.com>
2022-02-02 10:56:52 +0100

    adds final data for video

--- DummyData/sqlfiles/04_bootdb_project_entity.sql
+++ DummyData/sqlfiles/04_bootdb_project_entity.sql
@@ -1,6 +1,6 @@
-INSERT INTO bootdb.project_entity (id, creator, name) VALUES (100, '0fef539d-69be-4013-9380-6a12c3534c67', 'DKB');
-INSERT INTO bootdb.project_entity (id, creator, name) VALUES (200, '2375e026-d348-4fb6-b42b-891a76758d5d', 'Commerzbank');
-INSERT INTO bootdb.project_entity (id, creator, name) VALUES (300, '65119d5f-039e-404e-973e-f12c35fe9fef', 'Deutsche Bank');
-INSERT INTO bootdb.project_entity (id, creator, name) VALUES (400, '74c31a72-7c9e-4095-a1d6-ec208b57ff1a', 'Postbank');
-INSERT INTO bootdb.project_entity (id, creator, name) VALUES (500, '8299a81a-8162-4739-8b98-5bc41cf5f44c', 'Bank 5');
-INSERT INTO bootdb.project_entity (id, creator, name) VALUES (600, 'e8406f08-a78d-4531-baa5-c00c70caabbe', 'Bank 6');
+INSERT INTO bootdb.project_entity (id, creator, name) VALUES (1, '0fef539d-69be-4013-9380-6a12c3534c67', 'DKB');
+INSERT INTO bootdb.project_entity (id, creator, name) VALUES (2, '2375e026-d348-4fb6-b42b-891a76758d5d', 'Commerzbank');
+INSERT INTO bootdb.project_entity (id, creator, name) VALUES (3, '65119d5f-039e-404e-973e-f12c35fe9fef', 'Deutsche Bank');
+INSERT INTO bootdb.project_entity (id, creator, name) VALUES (4, '74c31a72-7c9e-4095-a1d6-ec208b57ff1a', 'Postbank');
+INSERT INTO bootdb.project_entity (id, creator, name) VALUES (5, '8299a81a-8162-4739-8b98-5bc41cf5f44c', 'Bank 5');
+INSERT INTO bootdb.project_entity (id, creator, name) VALUES (6, 'e8406f08-a78d-4531-baa5-c00c70caabbe', 'Bank 6');
