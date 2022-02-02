6c38ce7bb9a248c3ea3e4d993908b2d7c0e3679a
Tobi B <to-tb@gmx.de>
2022-02-02 03:54:46 +0100

    cleanup

--- frontend/quickcheck/src/pages/evaluationPage/EvaluationTable.jsx
+++ frontend/quickcheck/src/pages/evaluationPage/EvaluationTable.jsx
@@ -5,21 +5,24 @@ import Card from '../../components/Card';
 function RatingRow({ rating }) {
   return (
     <Card layerStyle="card_bordered" justifyContent="space-between" direction="column">
-      <Flex direction="row" justifyContent={'space-between'} w="full" mb={4}>
+      <Flex direction="row" justifyContent="space-between" w="full" mb={4}>
         <Text fontSize="xl">{rating.rating.criterion}</Text>
       </Flex>
-      <Flex direction="row" justifyContent={'space-between'} w="full" mb={2} alignItems={'center'}>
-        <VStack w="40%" alignItems={'left'} direction={'column'} spacing="0">
-          <Text fontSize={'sm'}>Answer</Text>
-          <Text variant="cell" minH="100px" align="left" placeholder={'Answer'}>
+      <Flex direction="row" justifyContent="space-between" w="full" mb={2} alignItems="center">
+        <VStack w="40%" alignItems="left" spacing={0}>
+          <Text fontSize="sm" align="left">
+            Answer
+          </Text>
+          <Text variant="cell" minH="5rem" align="left" placeholder="Answer">
             {rating.answer}
           </Text>
         </VStack>
         <Text variant="cell">{rating.score}</Text>
-        <VStack w="40%" alignItems={'left'} spacing="0">
-          <Text fontSize={'sm'}>Comment</Text>
-          <Text variant="cell" minH="100px" placeholder={'Comment'}>
-            {' '}
+        <VStack w="40%" alignItems="left" spacing={0}>
+          <Text fontSize="sm" align="left">
+            Comment
+          </Text>
+          <Text variant="cell" minH="5rem" placeholder="Comment" align="left">
             {rating.comment}
           </Text>
         </VStack>
@@ -30,7 +33,7 @@ function RatingRow({ rating }) {
 
 export default function EvaluationTable({ ratings }) {
   return (
-    <List spacing={2} direction="column" w="full" align="center">
+    <List spacing={2} w="full" align="center">
       {ratings.map((rating) => (
         <Flex gridGap={3}>
           <RatingRow rating={rating}></RatingRow>
