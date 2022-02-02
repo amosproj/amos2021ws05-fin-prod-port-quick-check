4c3e1084b5839804a02bedbe7085cfe4941f754c
Tobi B <to-tb@gmx.de>
2022-02-02 02:55:29 +0100

    refactor rating page

--- frontend/quickcheck/src/pages/ratingPage/RatingTable.jsx
+++ frontend/quickcheck/src/pages/ratingPage/RatingTable.jsx
@@ -1,55 +1,38 @@
-import { Flex, Input, Text, Spacer, List, Textarea, IconButton, HStack, VStack } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { Flex, Text, List, Textarea, IconButton, VStack } from '@chakra-ui/react';
+import React from 'react';
 import Card from '../../components/Card';
 import Selection from '../../components/Selection';
 import { useStoreActions, useStoreState } from 'easy-peasy';
-import { AttachmentIcon } from "@chakra-ui/icons";
+import { AttachmentIcon } from '@chakra-ui/icons';
 
-function RatingRow({ rating, onChangeScore }) {
-  const changeRatingAnswer = useStoreActions((actions) => actions.product_rating.changeAnswer);
-  const changeRatingCommet = useStoreActions((actions) => actions.product_rating.changeComment);
-  const productData = useStoreState((state) => state.product_rating.product);
+function RatingRow({ rating }) {
+  const updateRating = useStoreActions((actions) => actions.rating.updateRating);
 
-  const handleAnswerChange = (newRating) => {
-    if (rating.ratingID == 10) {
-      if (newRating.match("^[1-9]\\d{0,2}(?:,\\d{0,2}(?:,\\d{0,2})?)?$") == null) {
-        alert("Falsches Format")
-      }
-      else {
-        rating.answer = newRating;
-        changeRatingAnswer(rating);
-      }
-    }
-    else {
-      rating.answer = newRating;
-      changeRatingAnswer(rating);
-    }
+  const updateRatingAttribute = (key) => (value) => {
+    let change = {};
+    change[key] = value;
+    updateRating({ ratingID: rating.ratingID, ...change });
   };
-  const handleCommentChange = (newRating) => {
-    rating.comment = newRating;
-    changeRatingCommet(rating);
-  };
+
+  const handleUpdateComment = updateRatingAttribute('comment');
+  const handleUpdateAnswer = updateRatingAttribute('answer');
+  const handleUpdateScore = updateRatingAttribute('score');
 
   return (
-    <Card
-      layerStyle="card_bordered"
-      justifyContent="space-between"
-      // w={(parentID > 0) ? ' 90%' : 'full'}
-      direction="column"
-    >
-      <Flex direction="row" justifyContent={"space-between"} w="full" mb={4}>
-        <Text fontSize='xl'>{rating.rating.criterion}</Text>
-      </Flex>
-      <Flex direction="row" justifyContent={"space-between"} w="full" mb={2} alignItems={"center"}>
-        <VStack w="40%" alignItems={"left"} spacing="0">
-          <Text fontSize={"sm"}>Answer</Text>
+    <Card layerStyle="card_bordered" justifyContent="space-between" direction="column">
+      <Text fontSize="xl" mb={4} align="left" w="full">
+        {rating.rating.criterion}
+      </Text>
+      <Flex direction="row" justifyContent="space-between" w="full" mb={2} alignItems={'center'}>
+        <VStack w="40%" alignItems="left" spacing={0} mx={1}>
+          <Text fontSize={'sm'} align="left">
+            Answer
+          </Text>
           <Textarea
             align="center"
-            placeholder={'Answer'}
+            placeholder="Answer"
             value={rating.answer}
-            onChange={(e) => {
-              handleAnswerChange(e.target.value);
-            }}
+            onChange={(e) => handleUpdateAnswer(e.target.value)}
           />
         </VStack>
 
@@ -57,41 +40,35 @@ function RatingRow({ rating, onChangeScore }) {
           w="125px"
           options={['GERING', 'MITTEL', 'HOCH']}
           selected={rating.score}
-          onChange={onChangeScore}
-        >
-        </Selection>
-        <VStack w="40%" alignItems={"left"} spacing="0">
-          <Text fontSize={"sm"}>Comment</Text>
+          onChange={handleUpdateScore}
+        ></Selection>
+        <VStack w="40%" alignItems="left" spacing="0" mx={1}>
+          <Text fontSize="sm" align="left">
+            Comment
+          </Text>
           <Textarea
-            placeholder={'Comment'}
+            placeholder="Comment"
             value={rating.comment}
-            onChange={(e) => {
-              handleCommentChange(e.target.value);
-            }}
+            onChange={(e) => handleUpdateComment(e.target.value)}
           />
         </VStack>
         <IconButton variant="whisper" icon={<AttachmentIcon />} />
       </Flex>
-    </Card >
+    </Card>
   );
 }
 
-export default function RatingTable({ ratings, handleChange }) {
-  const handleScoreChange = (rating) => (newRating) => {
-    // This is a curried function in JS
-    let index = ratings.map((r) => r.rating.criterion).indexOf(rating.rating.criterion);
-    rating.score = newRating;
-    ratings[index] = rating;
-    handleChange(ratings);
-  };
+export default function RatingTable({ category }) {
+  const getRatingsByCategory = useStoreState((state) => state.rating.getRatingsByCategory);
 
   return (
-    <List spacing={2} direction="column" w="full" align="center">
-      {ratings.map((rating) => (
-        <Flex gridGap={3}>
-          <RatingRow rating={rating} onChangeScore={handleScoreChange(rating)}></RatingRow>
-        </Flex>
-      ))}
-    </List>
+    <>
+      <List spacing={2} direction="column" w="full" align="center">
+        {getRatingsByCategory(category).map((rating) => (
+          <RatingRow rating={rating} key={rating.ratingID}></RatingRow>
+        ))}
+      </List>
+      <p>{JSON.stringify(getRatingsByCategory(category))}</p>
+    </>
   );
 }
