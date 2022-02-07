0b0de26a86852403023a9d3cc9bbd2b032cc17b2
m4xrdk <maximilian.rodiek@campus.tu-berlin.de>
2022-02-07 17:52:07 +0100

    Java Doc for ProjectUserControllerIntegrationTest

--- service/src/test/java/com/tu/FinancialQuickCheck/IntegrationTests/ProjectUserControllerIntegrationTest.java
+++ service/src/test/java/com/tu/FinancialQuickCheck/IntegrationTests/ProjectUserControllerIntegrationTest.java
@@ -18,6 +18,9 @@ import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * The current test class verifies the functionalities of the Project User Controller
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProjectUserControllerIntegrationTest {
 
@@ -48,6 +51,9 @@ public class ProjectUserControllerIntegrationTest {
 
     UserEntity userNotAssignedToProject;
 
+    /**
+     * This annotated method should be executed before each invocation of @Test
+     */
     @BeforeEach
     public void initEach(){
         log.info("@BeforeEach - setup for Tests in ProjectUserControllerIntegrationTest.class");
@@ -83,6 +89,9 @@ public class ProjectUserControllerIntegrationTest {
         header.setContentType(MediaType.APPLICATION_JSON);
     }
 
+    /**
+     * The method should be run after every @Test
+     */
     @AfterEach
     public void reset() {
         repository.deleteAll();
@@ -92,6 +101,11 @@ public class ProjectUserControllerIntegrationTest {
         log.info("@AfterEach - db reset");
     }
 
+    /**
+     * This test tries to find a project users for a project by its ID, but the there are no users
+     *
+     * @result The status code that the project users wasn't found in database
+     */
     @Test
     public void test1_findProjectUsersByProjectId_expectResourceNotFound() {
         // delete all existing entries
@@ -107,6 +121,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * This test tries to get all project users from database by the project ID
+     *
+     * @result The status code that the project users were found in database
+     */
     @Test
     public void test2_findProjectUsersByProjectId_expectSuccess_200() {
         ResponseEntity<String> response = restTemplate.exchange(host + port + projects + project.id + users,
@@ -117,6 +136,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
+    /**
+     * This test tries to update a single project user by its ID, but there is no user
+     *
+     * @result The status code that the project user wasn't updated in database
+     */
     @Test
     public void test3_updateProjectUser_resourceNotFound_userID_singleUser(){
         String jsonString = "[{\"userID\":\"0fef539d-69be-4013-9380-6a12c3534c67\", \"role\":\"CLIENT\"}]";
@@ -129,6 +153,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * This test tries to update a multiple project user by its ID, but there are no project user with that IDs
+     *
+     * @result The status code that the project users were not found by their IDs
+     */
     @Test
     public void test4_updateProjectUse_resourceNotFound_userID_multipleUsers(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id +  "\", \"role\":\"CLIENT\"}," +
@@ -142,8 +171,13 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * This test tries to update a project user but the respective project ID wasn't found
+     *
+     * @result The status code that the project user was not found by its project ID
+     */
     @Test
-    public void test5_updateProjectUse_resourceNotFound_projectID(){
+    public void test5_updateProjectUser_resourceNotFound_projectID(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id +  "\", \"role\":\"CLIENT\"}]";
 
         ResponseEntity<String> response = restTemplate.exchange(host + port + projects + "0" + users,
@@ -154,6 +188,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * This test tries to update a project user but the user is not assigned to the project
+     *
+     * @result The status code that the project user is not assigned to the respective project
+     */
     @Test
     public void test6_updateProjectUser_badRequest_userNotAssignedToProject_singleUser(){
         String jsonString = "[{\"userID\":\" " + userNotAssignedToProject.id + "\", \"role\":\"CLIENT\"}]";
@@ -166,6 +205,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * This test tries to update multiple project users but the users are not assigned to the project
+     *
+     * @result The status code that the project users are not assigned to the respective project
+     */
     @Test
     public void test7_updateProjectUser_badRequest_userNotAssignedToProject_multipleUser(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\", \"role\":\"CLIENT\"}, " +
@@ -179,6 +223,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * This test tries to update a project user
+     *
+     * @result The status code that the project user is updated
+     */
     @Test
     public void test8_updateProjectUser_success_singleUser(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\", \"role\":\"ADMIN\"}]";
@@ -192,6 +241,11 @@ public class ProjectUserControllerIntegrationTest {
         //TODO: check output against input to db
     }
 
+    /**
+     * This test tries to update multiple project users
+     *
+     * @result The status code that the project users are updated
+     */
     @Test
     public void test9_updateProjectUser_success_multipleUser(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\", \"role\":\"ADMIN\"}, " +
@@ -206,6 +260,11 @@ public class ProjectUserControllerIntegrationTest {
         //TODO: check output against input to db
     }
 
+    /**
+     * This test tries to update a project user, but not the role
+     *
+     * @result The status code that the project user is updated except the role
+     */
     @Test
     public void test10_updateProjectUser_success_singleUser_withoutRole(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\"}]";
@@ -219,6 +278,11 @@ public class ProjectUserControllerIntegrationTest {
         //TODO: check output against input to db
     }
 
+    /**
+     * This test tries to update  project users, but not their roles
+     *
+     * @result The status code that the project users are updated except the roles
+     */
     @Test
     public void test11_updateProjectUser_success_multipleUser_withoutRole(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\"}, " +
@@ -233,6 +297,11 @@ public class ProjectUserControllerIntegrationTest {
         //TODO: check output against input to db
     }
 
+    /**
+     * This test tries to delete a project user but the user is not assigned to the project
+     *
+     * @result The status code that the user wasn't found for the project
+     */
     @Test
     public void test12_deleteProjectUser_resourceNotFound_userNotAssignedToProject_singleUser(){
         String jsonString = "[{\"userID\":\" " + userNotAssignedToProject.id + "\"}]";
@@ -245,6 +314,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * This test tries to delete multiple project users but the users are not assigned to the project
+     *
+     * @result The status code that the users weren't found for the project
+     */
     @Test
     public void test13_deleteProjectUser_resourceNotFound_userNotAssignedToProject_multipleUser(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\"}, " +
@@ -258,6 +332,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * This test tries to delete a user for a project, but the ID wasn't found
+     *
+     * @result The status code that project ID wasn't found, therefore the user wasn't deleted
+     */
     @Test
     public void test14_deleteProjectUser_badRequest_projectID(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\"}]";
@@ -270,18 +349,28 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * This test tries to delete users for a project, but the ID wasn't found
+     *
+     * @result The status code that project ID wasn't found, therefore the users weren't deleted
+     */
     @Test
-    public void test15_deleteProjectUser_badRequest_projectID(){
+    public void test15_deleteProjectUsers_badRequest_projectID(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\"}]";
 
         ResponseEntity<String> response = restTemplate.exchange(host + port + projects + 0 + users,
                 HttpMethod.DELETE, new HttpEntity<>(jsonString, header), String.class);
 
-        log.info("@Test 14 - deleteProjectUser_badRequest - projectID");
+        log.info("@Test 14 - deleteProjectUsers_badRequest - projectID");
         log.info("@Test 14 - Response Body: " + response.getBody());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * This test tries to delete a user for a project, but the user isn't assigned to the project
+     *
+     * @result The status code that the user for the project wasn't found because he wasn't assigned
+     */
     @Test
     public void test16_deleteProjectUser_resourceNotFound_userNotAssignedToProject_singleUser(){
         String jsonString = "[{\"userID\":\" " + userNotAssignedToProject.id + "\", \"role\":\"CLIENT\"}]";
@@ -294,6 +383,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * This test tries to delete users for a project, but the users aren't assigned to the project
+     *
+     * @result The status code that the users for the project weren't found because they weren't assigned
+     */
     @Test
     public void test17_deleteProjectUser_resourceNotFound_userNotAssignedToProject_multipleUser(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\"}, " +
@@ -307,6 +401,11 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * This method tries to delete a user from project
+     *
+     * @result The status code that the user is deleted from project
+     */
     @Test
     public void test18_deleteProjectUser_success_userNotAssignedToProject_multipleUser(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\"}]";
@@ -319,15 +418,20 @@ public class ProjectUserControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
+    /**
+     * This method tries to delete users from project
+     *
+     * @result The status code that the users are deleted from project
+     */
     @Test
-    public void test19_deleteProjectUser_success_userNotAssignedToProject_multipleUser(){
+    public void test19_deleteProjectUsers_success_userNotAssignedToProject_multipleUser(){
         String jsonString = "[{\"userID\":\" " + usersEntities.get(0).id + "\"}, " +
                 "{\"userID\":\" " + usersEntities.get(1).id + "\"}]";
 
         ResponseEntity<String> response = restTemplate.exchange(host + port + projects + project.id + users,
                 HttpMethod.DELETE, new HttpEntity<>(jsonString, header), String.class);
 
-        log.info("@Test 19 - deleteProjectUser_success - multiple users");
+        log.info("@Test 19 - deleteProjectUsers_success - multiple users");
         log.info("@Test 19 - Response Body: " + response.getBody());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
