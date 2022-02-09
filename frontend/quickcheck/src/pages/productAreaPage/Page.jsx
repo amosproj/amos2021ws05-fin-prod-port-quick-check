f862cd3cdcacaf8c23d6b4ca5a9832631e7cc73f
Andreas Ronellenfitsch <bn0122@partner.kit.edu>
2022-02-09 19:30:08 +0100

    Updated product area page documentation

--- frontend/quickcheck/src/pages/productAreaPage/Page.jsx
+++ frontend/quickcheck/src/pages/productAreaPage/Page.jsx
@@ -8,8 +8,19 @@ import AddProductButton from './AddProductButton';
 import ProductRow from './ProductRow';
 /**
  * Displays the product area page. 
- * The product area page contains products of a specific product area.
- * 
+ * The product area page contains a list of products for a specific product area. 
+ * The products are mapped to one list item each displayed by a ProductRow.
+ * Products can contain product variants which are nested in a product row.
+ * Product variants are mapped to a ProductVariant.
+ * The product area page links to the results page and for each product / product variant either to the
+ *    - economic rating page or
+ *    - complexity rating page or
+ *    - evaluation page
+ * The page contains an edit button which disappears and is replaced by confirm and cancel buttons once
+ * the user clicks it. Then the edit mode is activated and changes to product names and comments can be 
+ * made. Clicking on the then appearing confirm button sends the updates made to the backend and ends the 
+ * edit mode. Clicking on cancel aborts the updating and discards the changes made. The edit mode will be 
+ * switched off. 
  */
 export default function ProductOverview() {
   const items = useStoreState((state) => state.productList.items);
